cb95e0ea862f437f0694f69ec26e6c053574e13b
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-24 07:49:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.if_statements_continued
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2) 
+        { 
+            if(num1 > num2) 
+            {
+                return num1;
+            }
+            else 
+            { 
+                return num2;
+            }
+        }
+    }
+}
